e07775cc610a9e662ef4b2a4ad37d676df890846
Adrian <adrian_iskra@o2.pl>
2021-03-07 16:37:38 +0100

    jd

---
+++ request.py
@@ -0,0 +1,66 @@
+import requests
+import configparser
+import json
+import functions as func
+
+config = configparser.ConfigParser()
+config.read(func.CONFIG_FILE)
+
+CALLS = int(config['API']['calls'])
+DEBUG = int(config['API']['debug'])
+DEBUG = True if int(DEBUG) == 1 else False
+COLUMNS = ['long', 'lat', 'position-source']
+
+
+# => API DATA
+# LAT = str(40)
+# LON = str(-85)
+# => DISTANCE WILL ALWAYS BE 25 CUZ IN THIS API
+# => THE DISTANCE FROM GIVEN POINT CANNOT CHANGE ;<
+DIST = str(25)
+
+URL = config['API']['url']
+API_KEY = config['API']['api_key']
+HOST = config['API']['host']
+
+print('CURRENT CALLS: ', CALLS + 1)
+
+
+def get_flights() -> str:
+    if CALLS < 360 and DEBUG is False:
+
+        for destination in func.DESTINATIONS:
+            LAT, LON = func.DESTINATIONS[destination]
+            print(destination)
+
+            # => CREATE URL
+            url = URL + "lat/" + str(int(LAT)) + "/lon/" + str(int(LON)) + "/dist/" + DIST + "/"
+
+            headers = {
+                'x-rapidapi-key': API_KEY,
+                'x-rapidapi-host': HOST
+            }
+
+            response = requests.request("GET", url, headers=headers).json()
+
+            # => INCREMENT THE CALLS VALUE IN CONFIG
+            func.increment_calls(config, func.CONFIG_FILE)
+
+            # => SAVE TO FILENAME
+            result = func.change_json(response, func.JSON_FILE)
+
+            if result is False:
+                print("[ ERROR ] Json hasn't been saved! ")
+
+            # return response
+
+    # => LOAD FROM JSON FILE
+    elif DEBUG:
+        print('[  DEBUG MODE  ] [ LOAD FROM JSON FILE ]')
+
+        content = func.load_from_json(func.JSON_FILE)
+        return content
+
+    # => NO DEBUG NO API CALLS
+    else:
+        raise Exception('Cannot do more api calls! ')
---
+++ visualize.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+""" Description: visualize file - shows the map with current flights up the US """
+
+# => 3-RD PARTY IMPORTS
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import numpy as np
+
+# => IMPORTS
+import functions as func
+import request as req
+import extract
+
+
+def create_basemap(scatters: list, airports: dict):
+    figure = plt.figure(figsize=(16, 10))
+
+    MAP = Basemap(
+        projection='mill',
+        llcrnrlat=0,
+        urcrnrlat=80,
+        llcrnrlon=-170,
+        urcrnrlon=-20,
+        resolution='c'
+    )
+
+    MAP.drawcoastlines()
+
+    MAP.drawparallels(np.arange(-90, 90, 10), labels=[True, False, False, False])
+    MAP.drawparallels(np.arange(-180, 180, 30), labels=[True, False, False, False])
+
+    for iata, lat, lon in zip(airports['iata_code'][1:], airports['latitude_deg'][1:], airports['longitude_deg'][1:]):
+
+        MAP.scatter(
+            int(float(lon)), int(float(lat)),
+            latlon=True, s=15, c="w", marker=",", zorder=3,
+        )
+
+        # => SHOW opicao CODE OF EVERY PLANE THAT HAS THIS DATA
+        x, y = MAP(int(float(lon))+1, int(float(lat))-1)
+        plt.text(x, y, iata, fontsize=8, zorder=6, color='white')
+
+    for scatter, label in zip(scatters, labels):
+        print(scatter)
+
+        MAP.scatter(
+            int(float(scatter['lon'])), int(float(scatter['lat'])),
+            latlon=True, s=100, marker="2", c="y", zorder=8,
+        )
+
+        # => SHOW opicao CODE OF EVERY PLANE THAT HAS THIS DATA
+        # x, y = MAP(int(float(scatter['lon'])), int(float(scatter['lat'])))
+        # plt.text(x+10, y, label, fontsize=9, zorder=9, color='yellow')
+
+    # MAP.scatter(-100, 60, latlon=True, s=2000, marker='^')
+    # MAP.scatter(-50, 65, latlon=True, s=2000, marker='^')
+
+    # MAP.bluemarble()
+    MAP.drawmapboundary(fill_color='#040C0E')
+    MAP.drawcountries()
+    MAP.fillcontinents(color='#192e5b', lake_color='#040C0E')
+    MAP.drawstates()
+
+    # plt.savefig(func.SAVE_PATH)
+    plt.show(color='#fbc531')
+
+
+# -> TRUNCATE THE JSON FILE FOR NEW DATA
+func.write_to_json({"ac": []}, func.JSON_FILE)
+
+# => GET JSON DATA
+response = req.get_flights()
+data = func.load_from_json(func.JSON_FILE)
+
+new = extract.get_values(data, ["lat", "lon", "from", "to", "opicao"])
+labels = extract.get_labels(new, "opicao")
+create_basemap(new, func.US_AIRPORTS)
+
